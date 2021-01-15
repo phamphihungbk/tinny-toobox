3f16926e4450fa71467777fb1c5154b8bbc54306
Hung Pham <phamphihungbk@gmail.com>
2021-01-15 20:29:10 +0800

    update git commands

---
+++ README.md
@@ -0,0 +1,15 @@
+## How to use
+
+### If you have [fish](https://fishshell.com/)
+
+Just copy the whole thing inside main.sh expect `#!/usr/bin/env bash` and pass into terminal
+!['guide-1'](./src/img/guide-1.jpg)
+
+### If you just use normal terminate
+
+- chmod +x main.sh, grant execution permission for the script
+- ./main.sh, how to run the script
+
+## Reference document
+- [Linux commands](https://cheatography.com/davechild/cheat-sheets/linux-command-line/)
+- [Chmod document](https://gist.github.com/juanarbol/c44e736be70279c1fd5d68aa24f9d8be)
--- main.sh
+++ main.sh
@@ -1,19 +1,38 @@
-#!/bin/bash
-master='master'
-read command
-main(){
-    case ${command} in
-        "master")
-        git checkout ${master} && git pull --rebase ${master}
-        ;;
+#!/usr/bin/env bash
+#  ---------------------------------------------------------------------------
+#
+#  Description:  This file holds all my BASH configurations and aliases
+#
+#  Sections:
+#  1.  Linux Command Lines
+#  2.  Git Command Lines
+#
+#  ---------------------------------------------------------------------------
 
-        "brand")
-        read branch
-        git checkout -b ${branch}
-        ;;
+#   -----------------------------
+#   1. Linux Command Lines
+#   -----------------------------
+alias r="rm -rf"                            # r: Remove file
+alias ch="chmod"                            # ch: Grant permission for specific file or folder
+alias t='touch'                             # t: Create new file
+alias c='clear'                             # c: Clear terminal display
+alias .~="cd ~"                             # ~: Go Home
+alias .1='cd ../'                           # Go back 1 directory level
+alias .2='cd ../../'                        # Go back 2 directory levels
+alias .3='cd ../../../'                     # Go back 3 directory levels
+alias .4='cd ../../../../'                  # Go back 4 directory levels
+alias .5='cd ../../../../../'               # Go back 5 directory levels
+alias .6='cd ../../../../../../'            # Go back 6 directory levels
+alias l="ls -las"                           # List down all directory and hidden directory with size and permission
 
-        *)
-        echo -n "unknow"
-        ;;
-    esac
-}
+#   -----------------------------
+#   2. Git Command Lines
+#   -----------------------------
+alias gc="git clone"
+alias gs="git status"
+alias glo="git pull origin"
+alias gpo="git push origin"
+alias gc="git commit"
+alias ga="git add"
+alias grb="git rebase"
+alias gr="git rm"
