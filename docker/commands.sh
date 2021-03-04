cd04152e1de0ce67ba04b944c01a2ebde7199efa
Hung Pham <phamphihungbk@gmail.com>
2021-03-04 19:10:16 +0800

    docker commands

--- docker/commands.sh
+++ docker/commands.sh
@@ -2,3 +2,7 @@
 docker build -f "location file" -t cassandra .
 #Run docker instance from available image
 docker run --name "image of instance" -d "name image"
+#Tag local image with remote image
+docker tag local_image remote_image
+#Push image to remote server
+docker push image_name
