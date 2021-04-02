b2f72f12f6afab859ef3b6217d542d321d81ebfe
Hung Pham <phamphihungbk@gmail.com>
2021-04-02 23:18:46 +0800

    aggregate query from ES API

---
+++ es/common-query-api.sh
@@ -0,0 +1,18 @@
+# Like count and group by in SQL statement
+GET /my-index-000001/_search
+{
+  "aggs": {
+    "my-agg-name": {
+      "terms": {
+        "field": "my-field"
+      },
+      "aggs": {
+        "my-sub-agg-name": {
+          "avg": {
+            "field": "my-other-field"
+          }
+        }
+      }
+    }
+  }
+}
