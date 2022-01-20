1ad4814ef1b1c49f0792d3748d6872cd4e918e5b
Hung Pham <phamphihungbk@gmail.com>
2022-01-20 13:42:33 +0700

    chore: kill command

--- operation/commands.sh
+++ operation/commands.sh
@@ -5,6 +5,6 @@ dpkg-query -l
 #Find the crontab in instance
 crontab -e
 #Kill process with kill -9
-kill -SIGKILL [pid] | kill -9 [pid]
+kill -SIGKILL [pid] | kill -9 [pid] | kill -9 $(ps aux | grep [name] | awk '{print $2}')
 #Find PID(process id)
 ps -aux
